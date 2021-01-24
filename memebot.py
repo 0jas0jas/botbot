05b1103c9d570dabbe681ac8175e7e75abf7d826
Ojas Srivastava <ojas1807@gmail.com>
2021-01-24 13:56:52 +0530

    Update memebot.py

--- memebot.py
+++ memebot.py
@@ -4,10 +4,14 @@ import os
 client = discord.Client()
 
 banned_words = [' u ', ' U ', ' ur ', ' Ur ', ' UR ']
+
 all_Inputs = [
     " 'typo again?' -- Provide someone in need with a keybaord.",
     " 'mind blown' -- Oh your mind is blown? Tell me about it.",
-    " 'good stuff' -- Certainly the person has some GOOOOOOOD stuff."
+    " 'good stuff' -- Certainly the person has some GOOOOOOOD stuff.",
+    " 'mood kharab' -- Why do you ruin my mood?",
+    " NEVER GONNA SAY GOODBYE",
+    " //or you could try greeting me\\"
 ]
 no_inputs = len(all_Inputs)
 
@@ -28,22 +32,36 @@ async def on_message(message):
         for thingsToSay in range(no_inputs):
             await message.channel.send(all_Inputs[thingsToSay])
 
+    #greeting
+    if message.content.startswith('hey') or message.content.startswith('hello') or message.content.startswith('hi') or message.content.startswith('yo') or message.content.startswith('heyo') or message.content.startswith('Hello') or message.content.startswith('Hi') or message.content.startswith('Yo') or message.content.startswith('Heyo'):
+        await message.channel.send('HELLO!')
     #hey mr arnab
     if message.content.startswith('hey mr. arnab'):
-        await message.channel.send(file = discord.File('TIME.png'))
+        time = 'https://cdn3.bbcode0.com/uploads/2021/1/24/ec337d7a0af5b036e078117309bcb011-full.png'
+        await message.channel.send(time)
     #please don't use u
     if any(word in message.content for word in banned_words):
-        await message.channel.send(file = discord.File('pleaseno.png'))
+        pleaseno = 'https://pbs.twimg.com/media/EsV_mSAU0AM2PC1?format=png&name=small'
+        await message.channel.send(pleaseno)
     #user types typo again?
     if message.content.startswith('typo again?'):
-        await message.channel.send('https://media1.tenor.com/images/1fa1ceb9b698b9071fa3e5a1cc0224bb/tenor.gif?itemid=20073354')
+        typo = 'https://media1.tenor.com/images/1fa1ceb9b698b9071fa3e5a1cc0224bb/tenor.gif?itemid=20073354'
+        await message.channel.send(typo)
     #user types mind blown
     if message.content.startswith('mind blown'):
-        await message.channel.send('https://tenor.com/view/mind-blown-head-explode-amazing-shocking-wow-gif-19681784')
+        mindblown = 'https://tenor.com/view/mind-blown-head-explode-amazing-shocking-wow-gif-19681784'
+        await message.channel.send(mindblown)
     #user types good stuff
     if message.content.startswith('good stuff'):
-        await message.channel.send('https://tenor.com/view/boy-kid-computer-thumbs-up-face-gif-9548945')
-
-
+        thumbsup = 'https://tenor.com/view/boy-kid-computer-thumbs-up-face-gif-9548945'
+        await message.channel.send(thumbsup)
+    #mood kharab
+    if message.content.startswith('mood kharab'):
+        moodkharab = 'https://pbs.twimg.com/media/EsV_Mi-UcAA_vy5?format=jpg&name=360x360'
+        await message.channel.send(moodkharab)
+    #bye
+    if message.content.startswith('bye') or message.content.startswith('Bye') or message.content.startswith('Goodbye') or message.content.startswith('goodbye'):
+        bye = 'https://tenor.com/view/peace-out-bye-gif-10267883'
+        await message.channel.send(bye)
 #Token for bot    
 client.run('Nzk4OTY0ODc3NzYyODg3NzUy.X_8sBg.o86mF7Ac7XmBWHJXAgLJVwn4sEg')
